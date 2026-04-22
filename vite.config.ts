45086936a8a5e786f3fac3306e9903e571b7dd81
Mason Mitchell <masonmitchell@MacBook-Pro-76.local>
2026-04-21 19:12:07 -0500

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Portfolio-Site/', // Must match your GitHub repo name exactly
+  base: '/', 
   plugins: [react()],
 })
 
